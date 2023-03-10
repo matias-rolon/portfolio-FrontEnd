1b7b64fa4cb8a092a0f1b9037cbf1d4b748f86b1
Matias Rolon <matiasrolon1301@gmail.com>
2023-03-10 00:00:05 -0300

    Creacion del header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from "./components/login/login.component";
+import { PortfolioComponent } from "./components/portfolio/portfolio.component";
 
 const routes: Routes = [
+  {path: 'portfolio', component:PortfolioComponent},
   {path: 'login' , component:LoginComponent},
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HeaderComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
