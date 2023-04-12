e9bfdf1d58186c894b47cbf720d4537bb8601ad5
Matias Rolon <matiasrolon1301@gmail.com>
2023-04-12 13:28:25 -0300

    Creacion e implementacion del servico de login en el componente

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,8 +2,8 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Usuario } from 'src/app/models/usuario';
-import { SobreMiService } from 'src/app/services/sobre-mi.service';
+import { loginUser } from 'src/app/models/loginUser';
+import { LoginUserService } from 'src/app/services/login-user.service';
 
 @Component({
   selector: 'app-login',
@@ -15,21 +15,21 @@ export class LoginComponent implements OnInit {
   public formSubmit = false;  
   public valido = false
   public mostrarError = false
-  public usuario:Usuario | undefined;
+  public usuario:loginUser | undefined;
   public loginForm = this.fb.group({
     email:['', [Validators.required, Validators.email]],
     password:['', Validators.required]
   });
 
-  constructor(private user : SobreMiService, private router:Router, private fb: FormBuilder ) { }
+  constructor(private user : LoginUserService, private router:Router, private fb: FormBuilder ) { }
 
   ngOnInit(): void {
     this.getUser();    
   }
 
   public getUser():void{
-    this.user.getUser().subscribe({
-      next:(response:Usuario) =>{
+    this.user.getLoginUser().subscribe({
+      next:(response:loginUser) =>{
         this.usuario=response;
       },
       error:(error:HttpErrorResponse)=>{
---
+++ src/app/services/login-user.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { loginUser } from '../models/loginUser';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginUserService {
+  private apiServerUrl = environment.apiBaseUrl;
+
+  constructor(private http: HttpClient) { }
+
+  public getLoginUser():Observable<loginUser>{
+    return this.http.get<loginUser>(`${this.apiServerUrl}/loginUser/id/1`);
+  }
+}
